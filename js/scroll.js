194231d44c829a696eaa3579bb30e6933d7ef6d0
aquahsu0811 <bellems811@gmail.com>
2021-10-28 18:17:53 +0800

    update js file

--- js/scroll.js
+++ js/scroll.js
@@ -3,18 +3,25 @@ function t() {
       , e = window.innerHeight
       , r = t.offsetHeight
       , i = e / 2 - r / 2;
-    console.log("centerPos", i);
+    
+    console.log("inner:",e);
+    console.log("centerPos:", i);
+    console.log("offsetHeight:",r);
     t.style.setProperty("top", i + "px");
     /*var n = document.querySelector(".js-st-fixed-contentMarkerEnd")
       , s = r - document.querySelector(".js-st-fixed-contentMarkerEnd .js-st-fixed-contentMarkerInner").clientHeight + 18;
     n.style.setProperty("padding-bottom", s + "px"),
     setTimeout((function() {}
     ), 100)*/
-    var sfc = document.querySelector(".st-fixed-contentItem")
-    if(i > e/10 && i <e/50 )
+    var sfc = document.querySelector("#js-st-img1")
+    
+    if(i > e/10 )
     {
-        console.log(i > e/10);
-        sfc.style.setProperty("background", 'red');
+        sfc.style.setProperty("background", "red");
+        //sfc.style.setProperty("transform-origin", '50% 50%');
+        sfc.style.setProperty("z-index", -1);
+        sfc.style.setProperty("opacity", 1);
+        sfc.style.setProperty("visibility", "hidden");
         //$(".st-fixed-contentItem").css({'style':'background:red'});
     }
 }
